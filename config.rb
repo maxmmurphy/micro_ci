d0ab06ffc17061beb5483e7ef0a8f784c387fe81
John Eberly <jeberly@elctech.com>
2010-02-24 13:55:23 -0800

    FIXED: logic for checking whether poller is running

--- config.rb
+++ config.rb
@@ -1,5 +1,4 @@
 require 'yaml'
 require 'ostruct'
-
 PROJECT_CONFIG = YAML::load(File.open(SINATRA_ROOT + '/config/projects.yml'))
 Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each {|lib| require lib }
