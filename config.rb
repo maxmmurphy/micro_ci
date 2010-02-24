19a0fb2c5d4157f026ecc1121f89600f38a80707
Max Murphy <mmurphy@elctech.com>
2010-02-24 11:12:17 -0800

    ADDED: pagination after jeberly's many many many requests

--- config.rb
+++ config.rb
@@ -4,3 +4,30 @@ require 'ostruct'
 
 PROJECT_CONFIG = YAML::load(File.open(SINATRA_ROOT + '/config/projects.yml'))
 Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each {|lib| require lib }
+
+
+Array.class_eval do
+  def paginate(opts = {})
+    opts  = {:page => 1, :per_page => 15}.merge(opts)
+    WillPaginate::Collection.create(opts[:page], opts[:per_page], size) do |pager|
+      pager.replace self[pager.offset, pager.per_page].to_a
+    end
+  end
+end
+
+WillPaginate::ViewHelpers::LinkRenderer.class_eval do
+  protected
+  def url(page)
+    url = @template.request.url
+    if page == 1
+      # strip out page param and trailing ? if it exists
+      url.gsub(/page=[0-9]+/, '').gsub(/\?$/, '')
+    else
+      if url =~ /page=[0-9]+/
+        url.gsub(/page=[0-9]+/, "page=#{page}")
+      else
+        url + "?page=#{page}"
+      end
+    end
+  end
+end
--- init.rb
+++ init.rb
@@ -1,23 +1,31 @@
 #!/usr/bin/env ruby
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/vendor/gems/will_paginate/lib'
 SINATRA_ROOT = Dir.pwd + '/' + File.dirname(__FILE__)                                                                                                 
 require 'rubygems'
 require 'sinatra'
+require 'sinatra_more/markup_plugin'
 require 'haml'
+require File.dirname(__FILE__) + '/vendor/gems/will_paginate/lib/will_paginate'
+require File.dirname(__FILE__) + '/vendor/gems/will_paginate/lib/will_paginate/view_helpers/link_renderer'
+require File.dirname(__FILE__) + '/vendor/gems/will_paginate/lib/will_paginate/view_helpers/base'
+
 require 'config'
 
-  get '/' do
-    @projects = MicroCI.projects
-    haml :index
-  end
+include WillPaginate::ViewHelpers::Base
 
-  get '/projects/:project_name' do
-    project_name = params[:project_name]
-    @builds = Project.new(project_name).builds.sort! {|a,b| b.build_number <=> a.build_number}
-    haml :project
-  end
+get '/' do
+  @projects = MicroCI.projects
+  haml :index
+end
 
-  get '/projects/:project_name/:build' do
-    project_name = params[:project_name]
-    @build = Build.new(params[:build], project_name)
-    haml :build
-  end
+get '/projects/:project_name' do
+  project_name = params[:project_name]
+  @builds = Project.new(project_name).builds.sort! {|a,b| b.build_number <=> a.build_number}.paginate(:page => params[:page] || 1)
+  haml :project
+end
+
+get '/projects/:project_name/:build' do
+  project_name = params[:project_name]
+  @build = Build.new(params[:build], project_name)
+  haml :build
+end
