24815ad97e216ebf91a7af387c8f5fb5574f3f45
John Eberly <jeberly@elctech.com>
2010-02-24 10:54:22 -0800

    UPDATED: project.builds to sort properly UPDATED: readme to include known issues FIXED: cron.rb to load properly when ran outside project dir

--- lib/project.rb
+++ lib/project.rb
@@ -16,7 +16,7 @@ class Project
   end
   
   def builds
-    Dir.glob(SINATRA_ROOT + "/builds/#{self.name}/*").map{|b| Build.new(File.basename(b, ".build_log"), self.name)}
+    Dir.glob(SINATRA_ROOT + "/builds/#{self.name}/*").map{|b| Build.new(File.basename(b, ".build_log"), self.name)}.sort! {|a,b| a.build_number <=> b.build_number}
   end
   
   def status_changed?
